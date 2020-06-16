390ef04a69c3e4bcd598da314a8722750dbf16ab
zhishiwo <2025311667@qq.com>
2020-06-16 14:28:36 +0800

    'init'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 // 应用根组件
 import React, {Component} from 'react'
-import {Button, message} from 'antd'
-import {BrowserRouter, HashRouter} from "react-router-dom"
+import {message} from 'antd'
+import {BrowserRouter, HashRouter, Switch, Route} from "react-router-dom"
+
+import Login from './views/login/login.jsx' 
+import Admin from './views/admin/admin.jsx' 
 
 class App extends Component {
 
-  handClick = () =>{
-    message.success('成功了')
+  handleSubmit = e => {
+    //阻止表单的提交
+    e.preventDefault()
+    alert("发送登录的ajax请求")
   }
 
   render (){
     return (
-      <Button type='primary' onClick={this.handClick}>测试</Button>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Admin} />
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
--- src/views/login/login.jsx
+++ src/views/login/login.jsx
@@ -1,11 +1,49 @@
 import React, { Component } from 'react'
+import {Form, Icon, Input, Button} from 'antd'
 
-export default class login extends Component {
+import logo from './images/logo.png'
+import './login.less'
+
+
+const Item = Form.Item
+
+ class Login extends Component {
   render() {
     return (
-      <div>
-          Login
+      <div className="login">
+        <div className="login-header">
+          <img src={logo} alt="logo"/>
+          <h1>React项目：后台管理系统</h1>
+        </div>
+        <div className="login-content">
+          <h1>用户登录陆</h1>
+          
+          <Form onSubmit={this.handleSubmit} className="login-form">
+            <Item>
+              <Input
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                 placeholder="Username"
+              />
+            </Item>
+            <Form.Item>
+              <Input
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    type="password"
+                    placeholder="Password"
+                  />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" className="login-form-button">
+                登陆
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
       </div>
     )
   }
 }
+
+const WrapperForm = Form.create()(Login)
+
+export default WrapperForm
